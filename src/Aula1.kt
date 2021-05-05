7b6422ae442f4824c92f8d8a96b279d7e711aa29
sergio alves <sergioalves@MacBook-Air-de-sergio.local>
2021-05-05 20:00:07 -0300

    mensagem

---
+++ src/Aula1.kt
@@ -0,0 +1,74 @@
+
+fun main(){
+
+    // val - Imutavel
+    // var - Mutável
+//     val inteiro: Int = 1
+//     val inteiro2 = 2
+//     val numeroGrande = 3245
+//     val texto = "Sérgio Alves"
+//     val ehMaior = false
+//     val decimalMenor = 2.5
+//     val decimalMaior = 2.283485838
+
+//   println(2>1)
+//   println(2 == 1)
+
+// println("Hello World")
+
+//       val pets = listOf("Cachorro", "Gato", "Tartaruga", "Ramster")
+//    val petsMutavel = mutableListOf("Cachorro", "Gato", "Peixe", "Ramster")
+//       println(pets.last())
+// 	println(pets[2] +" "+ petsMutavel[0])
+//     petsMutavel.add("Lagarto")
+//     println(petsMutavel)
+
+
+
+//       val numero = 2
+
+//       if(numero < 3){
+//           println("menor que 3 ")
+//       }else if( numero > 3){
+//           println("maior que 3")
+//       }else if(numero ==1){
+//           println("numero é igual a 1")
+//       }else{
+//           println("Caiu no else")
+//       }
+
+
+//       when {
+//           numero < 3 -> println("menor que 3")
+//           numero < 3 -> println("maior que 3")
+//           numero == 1 -> println("igual a um")
+//           else -> println("Caiu no else")
+//       }
+
+
+
+//       for(num in 1..100){
+//           println(num)
+//       }
+
+
+// petsMutavel.forEach{ pet -> println(pet)} percorre o array e lista cada elemento
+
+
+
+// val x = 0
+// while(x < 2){
+//     println(x)// loop infinito
+// }
+
+
+    var x = 0
+    while(x <= 2){
+        println(x)
+        x++
+    }
+//
+//
+//
+//
+}
